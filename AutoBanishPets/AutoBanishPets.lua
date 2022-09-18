cb770defe5c80d4952af1987fc4950a8fc211aca
tdenc <tdenc1125@gmail.com>
2022-09-18 10:19:50 +0900

    0.5.3

--- AutoBanishPets/AutoBanishPets.lua
+++ AutoBanishPets/AutoBanishPets.lua
@@ -5,7 +5,7 @@ local AutoBanishPets = AutoBanishPets
 --INITIATE VARIABLES--
 ----------------------
 AutoBanishPets.name = "AutoBanishPets"
-AutoBanishPets.version = "0.5.2"
+AutoBanishPets.version = "0.5.3"
 AutoBanishPets.variableVersion = 8
 AutoBanishPets.defaultSettings = {
     ["notification"] = true,
